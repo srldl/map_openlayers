a83638abe49e7f5b60bd9c1ad1b35b5eb5650884
srldl <siri.uldal@gmail.com>
2018-03-07 12:24:11 +0100

    works EPSG:32761

--- bundle.js
+++ bundle.js
@@ -8,15 +8,15 @@ var map_openlayers = angular.module('map_openlayers',[]);
 
 
 map_openlayers.controller('MapCtrl', function($scope) {
-  var EPSG = 'EPSG:3031'; // EPSG:3031 - WGS 84 / Antarctic Polar Stereographic
+  var EPSG = 'EPSG:32761'; // EPSG:3031 - WGS 84 / Antarctic Polar Stereographic
 
-  proj4.defs(EPSG, "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs");
+  proj4.defs(EPSG, "+proj=stere +lat_0=-90 +lat_ts=-90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs");
 
        var projection = ol.proj.get(EPSG);
        console.log(projection);
 
-       //var projectionExtent = [-2623287.1531823575, -2623287.1531823575,  6623287.153182365, 6623287.153182365];
-       var origin = [-28567900, 32567900];
+      // var projectionExtent = [2000000.0000, -1405880.7174, 2000000.0000, 2000000.0000];
+       var origin = [-28567900,32567900];
     //   var resolutions = new Array(9);
     //   var matrixIds = new Array(9);
        var resolutions = [21674.7100160867,10837.35500804335,5418.677504021675,2709.3387520108377,1354.6693760054188,677.3346880027094,338.6673440013547,169.33367200067735,84.66683600033868,42.33341800016934];
@@ -37,7 +37,7 @@ map_openlayers.controller('MapCtrl', function($scope) {
       url: url,
       layer: 'Basisdata_Intern_NP_Antarktis_WMTS_3031',
       matrixSet: 'default028mm',
-      format: 'image%2Fjpgpng',
+      format: 'image%2Fjpg',
       projection: projection,
       tileGrid: new ol.tilegrid.WMTS({
         origin: origin,
@@ -57,7 +57,8 @@ map_openlayers.controller('MapCtrl', function($scope) {
     }
   }),
         view: new ol.View({
-          center: ol.proj.transform([2.5333, -89.9999], 'EPSG:4326', EPSG),
+          projection: projection,
+          center: ol.proj.transform([0, -90], 'EPSG:4326', EPSG),
           zoom: 4
         })
       });
